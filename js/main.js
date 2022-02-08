605ca9ef92250e6f21645168c09273f9a0aba81d
Artemonka27 <99112526+Artemonka27@users.noreply.github.com>
2022-02-09 00:34:43 +1000

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let burger = document.querySelector(".burger");
-let menu = document.querySelector(".header__menu");
-burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    menu.classList.toggle("active");
-    document.body.classList.toggle("lock");
-});
